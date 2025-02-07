6deb14c1c88bb2684a1134ab0e58ebbafcab1db2
Vi-011 <vickyg11@nycstudents.net>
2025-02-07 09:45:42 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+"""  #Strings are for represnting characters, names, words etc.
+name = "David"
+#interger represents whole numbers
+age = 14
+#Floats represents decimals
+wallet = 5.45
+#boolean represents true or false, used in evaluations
+graduated = False
+
+def add(x,y):
+    print(x+y)
+#input asks the use a questions and store their response as a value
+bill = float(input("what was the bill?"))
+# print(type(bill))
+# add(40, bill)
+  """
+#lists
+students = ["Joanna", "Devivid", "David", "other David", "Ethan"]
+ #similar to saying for i in range(5): print(students[i])
+print(students[4])
+for students in students:
+    print(students) 
+
+moneys = [1,2,3,4,5,6]
+total = 0
+for money in moneys: 
+    total = total + money
+    print
+
