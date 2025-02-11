b2e3a2f3a56910bfc858b49674b28d9522e02158
Vi-011 <vickyg11@nycstudents.net>
2025-02-11 10:16:00 -0500

    nothing again

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ wallet = 5.45
 # Boolean represents true or false, used in evaluations
 graduated = False
 
-def add(x, y):
+def add(x, y)
     print(x + y)
 
 # input asks the user a question and stores their response as a value
@@ -19,30 +19,25 @@ bill = float(input("What was the bill?"))
 students = ["Joanna", "Devivid", "David", "other David", "Ethan"]
 # Similar to saying for i in range(5): print(students[i])
 print(students[4])
-for student in students:
+for student in students
     print(student)
 
 moneys = [1, 2, 3, 4, 5, 6]
 total = 0
-for money in moneys:
+for money in moneys
     total = total + money
     print(total)
-
- """
-
-""" 
-temp = 3
-if temp > 68:
-    print('warm')
-elif temp == 68:
-    print('perfect')
-else:
-    print('cold')
  """
 
 
 
 
-sentence = input("Please enter a sentence: ")
 
-def count_words (Hello, may I have a sandwhich)
+sentence = input("Please enter a sentence: ").strip() 
+
+def count_words(sentence):
+    words = sentence.split() 
+    return len(words)
+
+word_count = count_words(sentence)
+print(f"The number of words in the sentence is: {word_count}")
