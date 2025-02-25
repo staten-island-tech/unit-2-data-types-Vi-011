415c4d42a2b92063f5ff3d5ec1ba41964c19d91a
Vi-011 <vickyg11@nycstudents.net>
2025-02-25 10:13:59 -0500

    factors of a number

--- app.py
+++ app.py
@@ -97,7 +97,13 @@ print(f"Total bill (including tip): ${total:.2f}")
     
  """
 
-number = float(input("number"))
-
+ 
+ 
+""" number = float(input("number"))
+factors = []
+for i in range(1, int(number) + 1):
+    if number % i == 0:
+        factors.append(i)
+        print("Factors of", int(number), "are:", factors) """
 
 
